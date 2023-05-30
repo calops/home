7bbcf057a0fd592085448a4281a04dde7da16ed8
Rémi Labeyrie <calops@tocards.net>
2023-05-30 09:45:30 +0200

    fix nixglwrap

--- lib/default.nix
+++ lib/default.nix
@@ -2,26 +2,7 @@
   lib,
   nixpkgs,
   ...
-}: let
-  mkScriptWrapper = {
-    pkg,
-    suffix,
-    text,
-  }:
-    nixpkgs.runCommand "${pkg.name}-${suffix}" {} ''
-      mkdir $out
-      ln -s ${pkg}/* $out
-      rm $out/bin
-      mkdir $out/bin
-      for bin in ${pkg}/bin/*; do
-        wrapped_bin=$out/bin/$(basename $bin)
-        cat >$wrapped_bin <<"EOF"
-      ${text}
-      EOF
-        chmod +x $wrapped_bin
-      done
-    '';
-in {
+}: {
   nixGlWrap = {
     config,
     pkg,
@@ -31,11 +12,15 @@ in {
       then "nixGLNvidia-530.41.03"
       else "nixGLIntel";
   in
-    mkScriptWrapper {
-      inherit pkg;
-      suffix = "nixGL";
-      text = ''
-        exec ${nixGlBin} $bin \$@"
-      '';
-    };
+    nixpkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      for bin in ${pkg}/bin/*; do
+       wrapped_bin=$out/bin/$(basename $bin)
+       echo "exec ${nixGlBin} $bin \$@" > $wrapped_bin
+       chmod +x $wrapped_bin
+      done
+    '';
 }
