312553fc264371ffdc06993ff602bced7744cfe9
Rémi Labeyrie <calops@tocards.net>
2023-06-21 11:26:53 +0200

    remove hyprland input as it is now part of nixpkgs

--- flake.nix
+++ flake.nix
@@ -7,11 +7,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland.url = "github:hyprwm/Hyprland";
-    hy3 = {
-      url = "github:outfoxxed/hy3";
-      inputs.hyprland.follows = "hyprland";
-    };
+    hy3.url = "github:outfoxxed/hy3";
     firefox-addons.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     nixgl.url = "github:guibou/nixGL";
@@ -28,10 +24,6 @@
       inputs.neovim-nightly-overlay.overlay
       inputs.nixgl.overlay
       inputs.nixd.overlays.default
-    ];
-
-    extraModules = [
-      inputs.hyprland.homeManagerModules.default
     ];
 
     mkLib = pkgs:
@@ -52,14 +44,12 @@
           config.allowUnfree = true;
           overlays = overlays;
         };
-        modules =
-          extraModules
-          ++ [
-            ./roles
-            ./colorschemes
-            ./machines/${machine}.nix
-            {home.stateVersion = "23.05";}
-          ];
+        modules = [
+          ./roles
+          ./colorschemes
+          ./machines/${machine}.nix
+          {home.stateVersion = "23.11";}
+        ];
         extraSpecialArgs = {
           inherit inputs;
           inherit configurationName;
